a0ad5b2b15e665c1ef376e1a93f373e4865994d6
Igaemas <sasha.deroubaix@icloud.com>
2022-06-29 14:54:37 +0200

    useless code deleted and todo list done

--- incident_cap.ps1
+++ incident_cap.ps1
@@ -1,7 +1,6 @@
 ﻿$CriticalCapacity = 90
 
-
-function Get-Disk-Info {
+function Disk-Cleaner {
 
 $GlobalDiskView = Get-WmiObject -Class Win32_LogicalDisk
 
@@ -17,31 +16,16 @@ $GlobalDiskView = Get-WmiObject -Class Win32_LogicalDisk
     Write-Host "	nom du disque : $DeviceName"
     Write-Host "	capatité disque : $SpaceDiskUsedPourcent%"
 
-
     If($SpaceDiskUsedPourcent -ge $CriticalCapacity)
     {
      	$CapacityState = "CRITICAL"
     } else {
       $CapacityState = "SAFE"
     }
-    	Write-Host "	état du disque : $CapacityState"
-		
-		  Get-User-Input
-	  }
-}
-
-function Get-User-Input {
-	$UserInput = Read-Host -Prompt 'voulez-vous procéder au nettoyage basique ? (y/n) : '
-	if(($UserInput -eq "y") -or ($UserInput -eq "yes") -or ($UserInput -eq "oui"))
-	{
-		Write-Host "c'est un oui"
-	} elseif(($UserInput -eq "n") -or ($UserInput -eq "no") -or ($UserInput -eq "non"))
-	{
-		Write-Host "ah bah non"
-	} else {
-		Get-User-Input
+    Write-Host "	état du disque : $CapacityState"
+	  Write-Host "  "
+	  
 	}
 }
 
-
-Get-Disk-Info
+Disk-Cleaner
