47d232016f08ee83385a8925f01523e8975de88b
Igaemas <sasha.deroubaix@icloud.com>
2022-07-01 10:57:09 +0200

    remplacement ToString() par [math]::Round()

--- incident_cap.ps1
+++ incident_cap.ps1
@@ -6,38 +6,39 @@ function Disk-Cleaner {
 
 	$DiskListArray = @()
 
-  Foreach($Disk in $GlobalDiskView){
-    $DiskSpace = ($Disk.Size/1GB).ToString('F0')
- 		$DiskFreeSpace = ($Disk.freespace/1GB).ToString('F0')
-   	$DeviceID = $Disk.DeviceID
-    $DeviceName = $Disk.Name
-    $DriveLetter = $DeviceID.Substring(0,$DeviceID.Length-1)
-    
-    $SpaceDiskUsedPourcent = (100 - ((($Disk.FreeSpace/1GB) * 100)/($Disk.Size/1GB))).ToString('F0')
 
-    Write-Host "disque $DeviceID :"
-    Write-Host "	nom du disque : $DeviceName"
-    Write-Host "	capatité disque : $SpaceDiskUsedPourcent%"
+    Foreach($Disk in $GlobalDiskView){
+      $DiskSpace = [math]::Round(($Disk.Size/1GB))
+ 		  $DiskFreeSpace = [math]::Round(($Disk.freespace/1GB))
+   	  $DeviceID = $Disk.DeviceID
+      $DeviceName = $Disk.Name
+      $DriveLetter = $DeviceID.Substring(0,$DeviceID.Length-1)
     
-    If($SpaceDiskUsedPourcent -ge $CriticalCapacity)
-    {
-     	$CapacityState = "CRITICAL"
-    } else {
-      $CapacityState = "SAFE"
-    }
-    Write-Host "	état du disque : $CapacityState"
-	  Write-Host "  "
+      $SpaceDiskUsedPourcent = [math]::Round((100 - ((($Disk.FreeSpace/1GB) * 100)/($Disk.Size/1GB))))
+
+      Write-Host "disque $DeviceID :"
+      Write-Host "	nom du disque : $DeviceName"
+      Write-Host "	capatité disque : $SpaceDiskUsedPourcent%"
+    
+      If($SpaceDiskUsedPourcent -ge $CriticalCapacity)
+      {
+     	  $CapacityState = "CRITICAL"
+      } else {
+        $CapacityState = "SAFE"
+      }
+      Write-Host "	état du disque : $CapacityState"
+	    Write-Host "  "
 	  
-    $DiskListArray += $DriveLetter
-    
-  }
-
+      $DiskListArray += $DriveLetter
+      Write-Host $DiskListArray
+    }
   write-host "Supression des Recycle.Bin"
+  write-host $DiskListArray
   foreach($DiskLetter in $DiskListArray){
 	  Clear-RecycleBin -Force -DriveLetter $DiskLetter
     Get-ChildItem "$DeviceID\Windows\SoftwareDistribution\Download\" 
     #| Remove-Item -recurse -force
-    Get-ChildItem "$DeviceID:\Windows\Temp\"
+    Get-ChildItem "$DeviceID\Windows\Temp\"
     #| Remove-Item -recurse -force
   }
 }
